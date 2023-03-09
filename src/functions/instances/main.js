cc469f9ea6be7b40f792c4b5bfef07ce59408191
Bacon <bacon.antife@gmail.com>
2023-03-09 20:47:24 -0300

    Feature: instances/kill-instance

--- src/functions/instances/main.js
+++ src/functions/instances/main.js
@@ -10,6 +10,12 @@ export const init = async (client) => {
 		if (text === '.instances') {
             const channel = client.channels.cache.get(message.channelId);
             channel.send(`I'm running on \`${Config.instance}\``);
+        } if (text.startsWith('.kill-instance ')) {
+            const instance = text.replace(/\.kill-instance\s*/, '').trim();
+            if (Config.instance === instance) {
+                await message.reply('Bye!');
+                process.exit(0);
+            }
         }
 	});
 };
