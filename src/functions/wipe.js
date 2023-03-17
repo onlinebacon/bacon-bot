709adbe6c0aa1a7b726f65b12becf054546e59d0
Bacon <bacon.antife@gmail.com>
2023-03-17 19:56:20 -0300

    Wipe: allows count argument

--- src/functions/wipe.js
+++ src/functions/wipe.js
@@ -3,9 +3,11 @@ import Config from '../config.js';
 export const init = (cli) => {
 	cli.onMessage(async (msg) => {
 		if (!msg.isFromRoot()) return;
-		if (msg.getText() !== '.wipe') return;
+		const text = msg.getText().trim();
+		if (!/^\.wipe(\s+\d+)?$/.test(text)) return;
+		const limitStr = text.replace(/^[^\s]+\s*/, '');
 		const channel = msg.getChannel();
-		await channel.wipe();
+		await channel.wipe(limitStr ? Number(limitStr) + 1: Infinity);
 		const tc = cli.getTextChannel(Config.reportChannelId);
 		tc.sendTextMessage(`Channel **${channel.getName()}** was wiped`);
 	});
--- src/wrappers/text-channel-wrapper.js
+++ src/wrappers/text-channel-wrapper.js
@@ -2,16 +2,18 @@ import ChannelAdapter from './channel-wapper.js';
 import MessageAdapter from './message-wrapper.js';
 
 export default class TextChannelAdapter extends ChannelAdapter {
-    async wipe() {
+    async wipe(count) {
         const channel = this.getChannelObject();
         for (;;) {
-            const messages = await channel.messages.fetch({ limit: 50 });
+            const messages = await channel.messages.fetch({ limit: Math.min(count, 50) });
             if (messages.size == 0) return;
             const promises = [];
             messages.forEach(message => {
                 promises.push(message.delete());
             });
             await Promise.all(promises);
+            count -= promises.length;
+            if (count <= 0) return;
         }
     }
     async sendTextMessage(text) {
