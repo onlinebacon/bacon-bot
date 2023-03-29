76be78cbcad02178c28d5736372ed43b22592bfd
Bacon <bacon.antife@gmail.com>
2023-03-29 12:25:26 -0300

    Flags support

--- src/lib/commands/Commands.js
+++ src/lib/commands/Commands.js
@@ -1,5 +1,9 @@
 const nameRegex = /^\.[a-z]+(-\w+)*/i;
-const flagRegex = /^#[a-z]+/i;
+const flagRegex = /^#[a-z]+(:\w+)?/i;
+
+const preventStyles = (str) => {
+	return str.replace(/([`*_~\\])/g, '\\$1');
+};
 
 class Command {
 	constructor({ name, description, syntax, examples, handler, argSep }) {
@@ -20,12 +24,12 @@ class Command {
 		}
 		if (this.examples) {
 			text += 'Examples:\n';
-			text += this.examples.map(s => `> ${s}`).join('\n');
+			text += this.examples.map(preventStyles).map(s => `> ${s}`).join('\n');
 		}
 		return ctx.msg.reply(text);
 	}
 	handleBadSyntax(ctx) {
-		return ctx.msg.reply(`Invalid syntax`);
+		return ctx.msg.reply(`**Invalid syntax**\nTry\n\`.${this.name} --help\``);
 	}
 }
 
@@ -51,10 +55,21 @@ class CommandManager {
 			await ctx.msg.reply(`Unrecognized command **${name}**`);
 			return;
 		}
-		if (text == '--help') {
+		if (text === '--help') {
 			return cmd.help(ctx);
 		}
-		const args = text ? (cmd.argSep ? text.split(cmd.argSep) : text) : [];
+		for (;;) {
+			let flag = text.match(flagRegex)?.[0];
+			if (flag == null) break;
+			text = text.substring(flag.length).trim();
+			flag = flag.substring(1);
+			if (!ctx.flagIsKnown(flag)) {
+				await ctx.msg.reply(`Unrecognized flag **${flag}**`);
+				return;
+			}
+			ctx.applyFlag(flag);
+		}
+		const args = cmd.argSep ? (text ? text.split(cmd.argSep) : []) : text;
 		await cmd.handler({ ctx, args });
 	}
 }
