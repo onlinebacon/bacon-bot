5b63d2cd96a16b015596af65ff1e5f8c71a9cf53
Bacon <bacon.antife@gmail.com>
2023-08-12 05:00:31 -0300

    Slight improvement in refraction calculation

--- src/lib/celnav/apply-refraction.js
+++ src/lib/celnav/apply-refraction.js
@@ -1,7 +1,6 @@
 import calcAltRefraction from './calc-alt-refraction.js'
 const removeRefraction = (alt) => alt - calcAltRefraction(alt);
 const applyRefraction = (trueAlt) => {
-	if (trueAlt > 89) return trueAlt;
 	let alt = trueAlt;
 	for (let i=0; i<10; ++i) {
 		let temp = removeRefraction(alt);
--- src/lib/celnav/calc-alt-refraction.js
+++ src/lib/celnav/calc-alt-refraction.js
@@ -1,6 +1,7 @@
 const cot = (deg) => 1/Math.tan(deg*(Math.PI/180));
 
 const calcAltRefraction = (alt) => {
+    if (alt > 89.9) return 0;
     return cot(alt + 7.31/(alt + 4.4))/60;
 };
 
