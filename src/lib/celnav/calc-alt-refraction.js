adf958a4a46a522dbd91e0af1fc9449d5d967fe3
Bacon <bacon.antife@gmail.com>
2023-08-26 17:20:07 -0300

    calc-alt-refraction args renamed

--- src/lib/celnav/calc-alt-refraction.js
+++ src/lib/celnav/calc-alt-refraction.js
@@ -1,13 +1,13 @@
 const cot = (deg) => 1/Math.tan(deg*(Math.PI/180));
 
-const refMultiplier = (pMb, tempC) => {
-    return pMb/1010*283/(273 + tempC);
+const refMultiplier = (milibars, celsius) => {
+    return milibars/1010*283/(273 + celsius);
 };
 
-const calcAltRefraction = (alt, pMb = 1010, tempC = 10) => {
+const calcAltRefraction = (alt, milibars = 1010, celsius = 10) => {
     if (alt > 89.9) return 0;
     const ref = cot(alt + 7.31/(alt + 4.4))/60;
-    const m = refMultiplier(pMb, tempC);
+    const m = refMultiplier(milibars, celsius);
     return ref*m;
 };
 
