344b818152f3d53b0c46dd4890a65aab2642e475
Bacon <bacon.antife@gmail.com>
2023-12-08 18:33:59 -0300

    Test command removed

--- src/commands/import-list.js
+++ src/commands/import-list.js
@@ -1,9 +1,7 @@
 import { muteUserButton, unmuteUserButton } from "./buttons/mute-user.js";
 import { micCtrlCommand } from "./commands/mic-ctrl.js";
-import { testCommand } from "./commands/test.js";
 
 export const commands = [
-	testCommand,
 	micCtrlCommand,
 ];
 
